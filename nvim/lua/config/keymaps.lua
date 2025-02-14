1b61aeadd77caa60dd5842ade637dc9dd317e417
Raj Shekhar Dev <shekhar.raj1610@gmail.com>
2025-02-14 16:14:15 +0530

    inital commit

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- use jj to escape in insert mode
+map("i", "jj", "<ESC>")
